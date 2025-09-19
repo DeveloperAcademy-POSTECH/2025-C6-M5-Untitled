4fbbc77fa3dd54d8c7e8d3098674e9c34fdba350
Ela <eunseolee90917@gmail.com>
2025-09-19 09:48:42 +0900

    Initial Commit

---
+++ BusRoad/BusRoadApp.swift
@@ -0,0 +1,17 @@
+//
+//  BusRoadApp.swift
+//  BusRoad
+//
+//  Created by Ella's Mac on 9/19/25.
+//
+
+import SwiftUI
+
+@main
+struct BusRoadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BusRoad/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BusRoad
+//
+//  Created by Ella's Mac on 9/19/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
