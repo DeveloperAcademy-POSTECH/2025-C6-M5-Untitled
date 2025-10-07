0c7d1792a32f52cada3547c50420b75b1fd72843
Ella <eunseolee90917@gmail.com>
2025-10-08 00:08:31 +0900

    [ui]#18 탑승중 카드 컴포넌트 제작
    - 탑승중 카드 컴포넌트 제작
    - progressbar도 같은파일 하단에 컴포넌트로 제작

---
+++ BusRoad/Component/OnRideView/OnRideCard.swift
@@ -0,0 +1,109 @@
+import SwiftUI
+
+struct OnRideCard: View {
+    
+    let busStopName: String
+    let remainingStops: Int
+    let progress: CGFloat
+    
+    
+    var body: some View {
+        
+        VStack {
+            
+            HStack {
+                VStack(alignment: .leading, spacing: 8) {
+                    Text(busStopName)
+                        .font(.prebold36)
+                        .foregroundStyle(remainingStops == 1 ? .subLight : .primaryHeavy)
+                    
+                    Text("정류장에서 내려야 해요.")
+                        .font(.prereg24)
+                        .foregroundStyle(remainingStops == 1 ? .subLight : .primaryHeavy)
+                }
+                
+                Spacer()
+            }
+            .padding(.horizontal, 40)
+                
+           
+            
+            //TODO: 여기에 로티,이미지 파일 들어가야함
+            /// 버스 탑승 중 이미지
+            Rectangle()
+                .cornerRadius(10)
+                .foregroundStyle(.subPoint)
+                .frame(width: 176, height: 146)
+                .padding(.horizontal, 84)
+                .padding(.top, 48)
+                .padding(.bottom, 50)
+            
+       
+                /// 남은 정류장 안내
+            HStack {
+                Spacer()
+                
+                HStack(spacing: 4) {
+                    Text("\(remainingStops)정류장")
+                        .font(.presemi20)
+                        .foregroundStyle(remainingStops == 1 ? .subLight : .primaryHeavy)
+                    
+                    Text("남았어요")
+                        .font(.prereg20)
+                        .foregroundStyle(remainingStops == 1 ? .subLight : .primaryHeavy)
+                }
+            }
+            .padding(.horizontal, 40)
+                
+                /// 남은정류장 progressbar
+                BusStopProgress(
+                    progress: progress,
+                    trackColor: remainingStops == 1 ? Color(.subHeavy) : Color(.primaryNormal),
+                    fillColor: remainingStops == 1 ? Color(.subNormal): Color(.primaryDisable)
+                )
+                .padding(.horizontal, 40)
+        }
+        .padding(.top, 60)
+        .padding(.bottom, 45)
+        .background(
+                   RoundedRectangle(cornerRadius: 20, style: .continuous)
+                       .fill(Color(remainingStops == 1 ? .primaryStrong : .primaryLight))
+               )
+        .padding(.horizontal, 24)
+    }
+}
+
+// MARK: - Progressbar
+
+struct BusStopProgress: View {
+    
+    let progress: CGFloat
+    var trackColor: Color = .primaryNormal
+    var fillColor: Color = .primaryDisable
+    
+    var body: some View {
+        
+        GeometryReader { geometry in
+            ZStack(alignment: .leading) {
+                Rectangle()
+                    .frame(width:geometry.size.width, height: 8)
+                    .cornerRadius(10)
+                    .foregroundStyle(trackColor)
+                
+                Rectangle()
+                    .frame(width: progress*geometry.size.width, height: 8)
+                    .cornerRadius(10)
+                    .foregroundStyle(fillColor)
+            }
+        }
+        .frame(height: 8)
+    }
+}
+
+#Preview {
+    OnRideCard(
+        busStopName: "Bus stop name",
+        remainingStops: 1,
+        progress: 0.9
+    )
+}
