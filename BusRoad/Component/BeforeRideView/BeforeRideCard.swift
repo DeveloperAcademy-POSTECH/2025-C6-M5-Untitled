bb80768e57af15cdb80b57b21bc5afa73d8c6dae
Ella <eunseolee90917@gmail.com>
2025-10-21 00:14:29 +0900

    [feat]#68 승차정류장 애니메이션 적용

--- BusRoad/Component/BeforeRideView/BeforeRideCard.swift
+++ BusRoad/Component/BeforeRideView/BeforeRideCard.swift
@@ -9,65 +9,70 @@ import SwiftUI
 
 
 struct BeforeRideCard: View {
-  
-  let waitingStopName: String
-  let waitingBusNO: String
-  @Binding var remainingStopsToBoarding: Int
-  let remainingTimeToBoarding: Int
-  
-  
-  var body: some View {
-      
-          ZStack {
-              Rectangle()
-                  .foregroundColor(remainingStopsToBoarding == 1 ? .primaryNormal : .subNormal)
-                  .cornerRadius(20)
-              
-              VStack(spacing: 20.wScaled) {
-                  VStack(spacing: 28.wScaled) {
-                      HStack {
-                          VStack(alignment: .leading, spacing: 8.wScaled) {
-                              Text(waitingStopName)
-                                  .font(.prebold36Scaled)
-                                  .foregroundStyle(remainingStopsToBoarding == 1 ? .subLight : .primaryHeavy)
-                              
-                              Text("정류장에서 타야 해요.")
-                                  .font(.prereg24Scaled)
-                                  .foregroundStyle(remainingStopsToBoarding == 1 ? .subLight : .primaryHeavy)
-                          }
-                          Spacer()
-                      }
-                      
-                      HStack{
-                          Text("\(waitingBusNO)")
-                              .font(.presemi32Scaled)
-                              .foregroundStyle(
+    
+    let waitingStopName: String
+    let waitingBusNO: String
+    @Binding var remainingStopsToBoarding: Int
+    let remainingTimeToBoarding: Int
+    
+    
+    var body: some View {
+        
+        ZStack {
+            Rectangle()
+                .foregroundColor(remainingStopsToBoarding == 1 ? .primaryNormal : .subNormal)
+                .cornerRadius(20)
+            
+            VStack(spacing: 20.wScaled) {
+                VStack(spacing: 28.wScaled) {
+                    HStack {
+                        VStack(alignment: .leading, spacing: 8.wScaled) {
+                            MarqueeText(
+                                text: waitingStopName,
+                                font: .presemi32Scaled,
+                                uiFont: .presemi32Scaled,
+                                startDelay: 1.0,
+                                alignment: .leading
+                            )
+                            .foregroundStyle(remainingStopsToBoarding == 1 ? .subLight : .primaryHeavy)
+                            
+                            Text("정류장에서 타야 해요.")
+                                .font(.prereg24Scaled)
+                                .foregroundStyle(remainingStopsToBoarding == 1 ? .subLight : .primaryHeavy)
+                        }
+                        Spacer()
+                    }
+                    
+                    HStack{
+                        Text("\(waitingBusNO)")
+                            .font(.presemi32Scaled)
+                            .foregroundStyle(
                                 remainingStopsToBoarding == 1 ? .primaryNormal: .subLight
-                              )
-                              .padding(.horizontal, 8.wScaled)
-                              .padding(.vertical, 4.wScaled)
-                              .background(
+                            )
+                            .padding(.horizontal, 8.wScaled)
+                            .padding(.vertical, 4.wScaled)
+                            .background(
                                 Rectangle()
                                     .foregroundColor(
                                         remainingStopsToBoarding == 1 ? .subNormal : .primaryHeavy
                                     )
                                     .cornerRadius(15)
-                              )
-                          
-                          Spacer()
-                      }
-                  }
-                  
-                  //TODO: 여기에 로티,이미지 파일 들어가야함
-                  Rectangle()
-                      .cornerRadius(10)
-                      .foregroundStyle(.subPoint)
-                      .frame(width: 200.wScaled, height: 200.wScaled)
-                     
-              }
-              .padding(.horizontal, 40.wScaled)
-      }
-  }
+                            )
+                        
+                        Spacer()
+                    }
+                }
+                
+                //TODO: 여기에 로티,이미지 파일 들어가야함
+                Rectangle()
+                    .cornerRadius(10)
+                    .foregroundStyle(.subPoint)
+                    .frame(width: 200.wScaled, height: 200.wScaled)
+                
+            }
+            .padding(.horizontal, 40.wScaled)
+        }
+    }
 }
 
 #Preview {
