48811252dc8cdac9b494c2e2297d86222c002bd1
llj72 <85468841+llj72@users.noreply.github.com>
2025-10-21 12:19:19 +0900

    [ui]#70 ui 수정
    - congrats 페이지 구현
    - 전체 경로 컴포넌트(도보만 있는 경우) Ui 수정
    - routecard 인디케이터 추가

--- BusRoad/Component/RouteSuggestionView/RouteCardSlide.swift
+++ BusRoad/Component/RouteSuggestionView/RouteCardSlide.swift
@@ -16,38 +16,50 @@ struct RouteCardSlide: View {
 //    private let cardWidth: CGFloat = 305.wScaled
         
     var body: some View {
+      VStack(spacing: 8) {
         ZStack {
           if errorMessage != nil {
             RouteErrorCard(viewModel: viewModel)
-            } else if let routes {
-                ZStack {
-                    ForEach(Array(routes.enumerated()), id: \.element.id) { index, item in
-                        let relativeIndex: CGFloat = CGFloat(index - currentIndex)
-                        RouteCard(journey: item, index: index)
-                            .offset(x: relativeIndex * 270.wScaled)
-                            .scaleEffect(relativeIndex == 0 ? 1.0 : 0.9)
-                            .opacity(relativeIndex == 0 ? 1.0 : 0.3)
-                            .zIndex(Double(routes.count) - Double(abs(index - currentIndex)))
-                            .animation(.spring(), value: currentIndex)
-                    }
-                }
-                .gesture(
-                    DragGesture()
-                        .onEnded { value in
-                            if abs(value.translation.width) < 50 {
-                                return
-                            }
-                            
-                            if value.translation.width > 0 {
-                                currentIndex = max(0, currentIndex - 1)
-                            } else {
-                                currentIndex = min(routes.count - 1, currentIndex + 1)
-                            }
-                        }
-                )
-            } else {
-                ProgressRouteCard()
+          } else if let routes {
+            ZStack {
+              ForEach(Array(routes.enumerated()), id: \.element.id) { index, item in
+                let relativeIndex: CGFloat = CGFloat(index - currentIndex)
+                RouteCard(journey: item, index: index)
+                  .offset(x: relativeIndex * 270.wScaled)
+                  .scaleEffect(relativeIndex == 0 ? 1.0 : 0.9)
+                  .opacity(relativeIndex == 0 ? 1.0 : 0.3)
+                  .zIndex(Double(routes.count) - Double(abs(index - currentIndex)))
+                  .animation(.spring(), value: currentIndex)
+              }
             }
+            .gesture(
+              DragGesture()
+                .onEnded { value in
+                  if abs(value.translation.width) < 50 {
+                    return
+                  }
+                  
+                  if value.translation.width > 0 {
+                    currentIndex = max(0, currentIndex - 1)
+                  } else {
+                    currentIndex = min(routes.count - 1, currentIndex + 1)
+                  }
+                }
+            )
+          } else {
+            ProgressRouteCard()
+          }
         }
+        
+        if let routes = routes, routes.count > 1 {
+          HStack {
+            ForEach(0..<routes.count, id: \.self) { index in
+              Circle()
+                .fill(index == currentIndex ? Color.greyStrong : Color.greyStrong.opacity(0.3))
+                .frame(width: 8, height: 8)
+            }
+          }
+        }
+      }
     }
 }
--- BusRoad/Feature/Congrats/CongratsView.swift
+++ BusRoad/Feature/Congrats/CongratsView.swift
@@ -8,13 +8,76 @@
 import SwiftUI
 
 struct CongratsView: View {
-    @EnvironmentObject private var coordinator: NavigationCoordinator
-    var body: some View {
-        VStack {
-            TopBar(isMoving: true) { coordinator.popToRoot() }
-            Spacer()
-            Text("Congratulations!")
-            Spacer()
+  @EnvironmentObject private var coordinator: NavigationCoordinator
+  @State private var isAnimating = false
+  
+  @State private var journey: Journey? = JourneyManager.shared.selectedJourney
+  
+  var body: some View {
+    ZStack {
+      Color(.primarywhite)
+        .ignoresSafeArea()
+      
+      VStack(spacing: 0){
+        
+        VStack(spacing: 32) {
+          TopBar(isMoving: true) { coordinator.popToRoot() }
+            .padding(.horizontal, 8)
+          
+          if let journey {
+              WholeJourney(
+                  journey: journey,
+                  journeyIndex: journey.nodes.count - 1,
+                  isBeforeRide: false
+              )
+              .padding(.horizontal, 32)
+            }
+          
+          LineDivider()
         }
+        .frame(height: 144)
+        ZStack {
+          Color(.background)
+            .ignoresSafeArea()
+          VStack(alignment: .leading) {
+            Spacer()
+            if let destination = JourneyManager.shared.destination {
+              Text(destination.name)
+                .font(.presemi36)
+                .foregroundColor(.primaryHeavy)
+            }
+            Spacer()
+            
+            HStack{
+              Spacer()
+              Image(systemName: "checkmark.circle.fill")
+                .font(.system(size: 148, weight: .bold))
+                .foregroundColor(.subStrong)
+                .rotation3DEffect(
+                  .degrees(isAnimating ? 360 : 0),
+                  axis: (x: 0, y: 1, z: 0)
+                )
+                .onAppear {
+                  withAnimation(.easeOut(duration: 2.0)) {
+                    isAnimating = true
+                  }
+                }
+              Spacer()
+            }
+            Spacer()
+            
+            Text("도착")
+              .font(.presemi32)
+              .foregroundColor(.primaryHeavy)
+            Text("했어요!")
+              .font(.prereg32)
+              .foregroundColor(.primaryHeavy)
+              .padding(.bottom, 80)
+          }
+          .padding(.horizontal,30)
+        }
+      }
     }
+  }
+  
 }
