5ace370b031d37b73599790b3d96ba55cac251ee
minjeongkx <2qqqaa@naver.com>
2025-10-22 21:51:56 +0900

    [QA] 알러트 수정
    도보 뷰 하단 버튼 알러트
    x버튼 알러트 디자인 수정

--- BusRoad/Component/StopNavigationAlert.swift
+++ BusRoad/Component/StopNavigationAlert.swift
@@ -16,34 +16,30 @@ struct StopNavigationAlert: View {
     if isPresented {
       ZStack {
         Color.black
+              .opacity(0.5)
           .ignoresSafeArea()
-        
-        Rectangle()
-          .frame(width: 300.wScaled, height: 199.wScaled)
-          .cornerRadius(35)
-          .foregroundColor(Color.background)
-        VStack(alignment:.center, spacing: 16.wScaled){
+        VStack(alignment:.center){
           Text("경로 안내를 종료할까요?")
             .font(.presemi24Scaled)
             .foregroundColor(.primary)
-            .padding(.leading, 8.wScaled)
-          Text("페이지를 종료하면 경로 안내가\n종료돼요.")
+            .padding(.top, 18.wScaled)
+            .padding(.bottom, 10.wScaled)
+          Text("페이지를 나가면\n경로 안내가 종료돼요.")
             .font(.prereg20Scaled)
             .foregroundColor(.primary)
-            .lineSpacing(5.wScaled)
-            .multilineTextAlignment(.leading)
-            .padding(.leading, 8.wScaled)
-          HStack(spacing: 16.wScaled){
+           .multilineTextAlignment(.center)
+           .padding(.bottom, 24.wScaled)
+          HStack(spacing: 10.wScaled){
             Button{
               isPresented = false
             } label:{
               ZStack{
                 Rectangle()
                   .cornerRadius(100)
-                  .foregroundColor(Color.primaryLight)
-                  .frame(width: 128.wScaled, height: 48.wScaled)
+                  .foregroundColor(Color.greybutton.opacity(0.2))
+                  .frame(width: 139.wScaled, height: 48.wScaled)
                 Text("닫기")
-                  .foregroundColor(Color.greyStrong)
+                  .foregroundColor(Color.black)
                   .font(.premed20Scaled)
               }
             }
@@ -55,7 +51,7 @@ struct StopNavigationAlert: View {
                 Rectangle()
                   .cornerRadius(100)
                   .foregroundColor(Color.cancelbutton)
-                  .frame(width: 128.wScaled, height: 48.wScaled)
+                  .frame(width: 139.wScaled, height: 48.wScaled)
                 Text("종료하기")
                   .foregroundColor(Color.subLight)
                   .font(.premed20Scaled)
@@ -63,7 +59,17 @@ struct StopNavigationAlert: View {
             }
           }
         }
-        .padding()
+        .padding(.vertical, 15.wScaled)
+        .frame(width: 320.wScaled)
+        .background(
+            RoundedRectangle(cornerRadius: 35)
+            .fill(.regularMaterial)
+            .overlay(
+              RoundedRectangle(cornerRadius: 35)
+                .stroke(Color.primarywhite, lineWidth: 0.5)
+            )
+        )
+          
       }
       .background(.clear)
     }
