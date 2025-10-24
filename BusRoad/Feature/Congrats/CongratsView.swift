382c5b6545bbd3475891b49332cee6fea46647ab
NanPark <nemyrmain@gmail.com>
2025-10-24 09:20:39 +0900

    [style] congrats뷰 애니메이션 수정

--- BusRoad/Feature/Congrats/CongratsView.swift
+++ BusRoad/Feature/Congrats/CongratsView.swift
@@ -8,129 +8,152 @@
 import SwiftUI
 
 struct CongratsView: View {
-  @EnvironmentObject private var coordinator: NavigationCoordinator
-  @State private var showArrivalConfirmation = false
-  @State private var isAnimating = false
-  
-  @State private var journey: Journey? = JourneyManager.shared.selectedJourney
-  
-  var body: some View {
-    ZStack {
-      Color(.primarywhite)
-        .ignoresSafeArea()
-      
-      VStack(spacing: 0.wScaled){
-        
-        VStack(spacing: 32.wScaled) {
-          TopBar(isMoving: true) { coordinator.popToRoot() }
-            .padding(.horizontal, 8)
-          
-          if let journey {
-              WholeJourney(
-                  journey: journey,
-                  journeyIndex: journey.nodes.count - 1,
-                  isBeforeRide: false
-              )
-              .padding(.horizontal, 32)
-            }
-          
-          LineDivider()
-        }
-        .frame(height: 144)
+    @EnvironmentObject private var coordinator: NavigationCoordinator
+    @State private var showArrivalConfirmation = false
+    @State private var isAnimating = false
+    @State private var showMarquee = false
+    
+    @State private var journey: Journey? = JourneyManager.shared.selectedJourney
+    
+    var body: some View {
         ZStack {
-          Color(.background)
-            .ignoresSafeArea()
+            Color(.primarywhite)
+                .ignoresSafeArea()
             
-            VStack(alignment: .leading) {
-              Spacer()
-              if let destination = JourneyManager.shared.destination {
-                ZStack{
-                  MarqueeText(
-                    text: destination.name,
-                    font: .presemi36Scaled,
-                    uiFont: .presemi36Scaled,
-                    startDelay: 1.0,
-                    alignment: .leading
-                  )
-                  .foregroundColor(Color.primaryHeavy)
-                }
-                .offset(y: showArrivalConfirmation ? 150.wScaled : 0) // 👈 아래로 140 이동
-                  .animation(.easeOut(duration: 1.0), value: showArrivalConfirmation)
-              }
-              Spacer()
-              
-              if showArrivalConfirmation {
-                ArrivalConfirmation(showArrivalConfirmation: $showArrivalConfirmation)
-              } else {
-                HStack{
-                  Spacer()
-                  Image(systemName: "checkmark.circle.fill")
-                    .font(.system(size: 148.wScaled, weight: .bold))
-                    .foregroundColor(.subStrong)
-                    .rotation3DEffect(
-                      .degrees(isAnimating ? 360 : 0),
-                      axis: (x: 0, y: 1, z: 0)
-                    )
-                    .onAppear {
-                      withAnimation(.easeOut(duration: 2.0)) {
-                        isAnimating = true
-                      }
-                      DispatchQueue.main.asyncAfter(deadline: .now() + 6.0) {
-                        showArrivalConfirmation = true
-                      }
-                    }
-                  Spacer()
-                }
-                Spacer()
+            VStack(spacing: 0.wScaled){
                 
-                Text("도착")
-                  .font(.presemi32Scaled)
-                  .foregroundColor(.primaryHeavy)
-                Text("했어요!")
-                  .font(.prereg32Scaled)
-                  .foregroundColor(.primaryHeavy)
-                  .padding(.bottom, 80.wScaled)
-              }
+                VStack(spacing: 32.wScaled) {
+                    TopBar(isMoving: true) { coordinator.popToRoot() }
+                        .padding(.horizontal, 8)
+                    
+                    if let journey {
+                        WholeJourney(
+                            journey: journey,
+                            journeyIndex: journey.nodes.count - 1,
+                            isBeforeRide: false
+                        )
+                        .padding(.horizontal, 32)
+                    }
+                    
+                    LineDivider()
+                }
+                .frame(height: 144)
+                ZStack {
+                    Color(.background)
+                        .ignoresSafeArea()
+                    
+                    VStack(alignment: .leading) {
+                        Spacer()
+                        
+                        if let destination = JourneyManager.shared.destination {
+                            Group {
+                                if showMarquee {    // 1.5초 후
+                                    MarqueeText(
+                                        text: destination.name,
+                                        font: .presemi36Scaled,
+                                        uiFont: .presemi36Scaled,
+                                        startDelay: 1.0,
+                                        alignment: .leading
+                                    )
+                                    .offset(y: showArrivalConfirmation ? 150.wScaled : 0)
+                                    .foregroundColor(Color.primaryHeavy)
+                                } else {    // 처음
+                                    Group {
+                                        MarqueeText(
+                                            text: destination.name,
+                                            font: .presemi36Scaled,
+                                            uiFont: .presemi36Scaled,
+                                            startDelay: 1.0,
+                                            alignment: .leading
+                                        )
+                                        .foregroundColor(Color.primaryHeavy)
+                                    }
+                                    .offset(y: showArrivalConfirmation ? 150.wScaled : 0)
+                                    .animation(.easeOut(duration: 1.0), value: showArrivalConfirmation)
+                                }
+                            }
+                            .onAppear {
+                                DispatchQueue.main.asyncAfter(deadline: .now() + 6.0 + 1.5) {
+                                    showMarquee = true
+                                    print("[DEBUG] showMarquee")
+                                }
+                            }
+                        }
+                        
+                        Spacer()
+                        
+                        if showArrivalConfirmation {
+                            ArrivalConfirmation(showArrivalConfirmation: $showArrivalConfirmation)
+                        } else {
+                            HStack{
+                                Spacer()
+                                Image(systemName: "checkmark.circle.fill")
+                                    .font(.system(size: 148.wScaled, weight: .bold))
+                                    .foregroundColor(.subStrong)
+                                    .rotation3DEffect(
+                                        .degrees(isAnimating ? 360 : 0),
+                                        axis: (x: 0, y: 1, z: 0)
+                                    )
+                                    .onAppear {
+                                        withAnimation(.easeOut(duration: 2.0)) {
+                                            isAnimating = true
+                                        }
+                                        DispatchQueue.main.asyncAfter(deadline: .now() + 6.0) {
+                                            showArrivalConfirmation = true
+                                        }
+                                    }
+                                Spacer()
+                            }
+                            Spacer()
+                            
+                            Text("도착")
+                                .font(.presemi32Scaled)
+                                .foregroundColor(.primaryHeavy)
+                            Text("했어요!")
+                                .font(.prereg32Scaled)
+                                .foregroundColor(.primaryHeavy)
+                                .padding(.bottom, 80.wScaled)
+                        }
+                    }
+                    .padding(.horizontal,30.wScaled)
+                }
             }
-            .padding(.horizontal,30.wScaled)
-          }
-      }
+        }
     }
-  }
-  
+    
 }
 
 struct ArrivalConfirmation: View {
-  @EnvironmentObject private var coordinator: NavigationCoordinator
-  @Binding var showArrivalConfirmation: Bool
-  
-  var body: some View {
-    VStack(alignment: .leading){
-      Spacer()
-      Text("목적지에 도착했어요.")
-        .font(.prereg32Scaled)
-        .foregroundColor(.primaryHeavy)
-      Spacer()
-      HStack{
-        Spacer()
-        Button {
-          coordinator.popToRoot()
-          showArrivalConfirmation = false
-        } label: {
-          ZStack{
-            Rectangle()
-              .cornerRadius(20)
-              .foregroundColor(.subStrong)
-              .frame(width: 240.wScaled, height: 75.wScaled)
-            Text("확인")
-              .font(.premed32Scaled)
-              .foregroundColor(.subLight)
-          }
-        }
-        Spacer()
-      }
-      .padding(.bottom, 20)
-    }
+    @EnvironmentObject private var coordinator: NavigationCoordinator
+    @Binding var showArrivalConfirmation: Bool
     
-  }
+    var body: some View {
+        VStack(alignment: .leading){
+            Spacer()
+            Text("목적지에 도착했어요.")
+                .font(.prereg32Scaled)
+                .foregroundColor(.primaryHeavy)
+            Spacer()
+            HStack{
+                Spacer()
+                Button {
+                    coordinator.popToRoot()
+                    showArrivalConfirmation = false
+                } label: {
+                    ZStack{
+                        Rectangle()
+                            .cornerRadius(20)
+                            .foregroundColor(.subStrong)
+                            .frame(width: 240.wScaled, height: 75.wScaled)
+                        Text("확인")
+                            .font(.premed32Scaled)
+                            .foregroundColor(.subLight)
+                    }
+                }
+                Spacer()
+            }
+            .padding(.bottom, 20)
+        }
+        
+    }
 }
