5d2256ab42dc15d3c36683ea61825fa68b087042
Ella <eunseolee90917@gmail.com>
2025-10-27 18:19:40 +0900

    [refactor]#133 MainSearchView, ViewModel 정리
    - View의 Helper 메서드들을 ViewModel로 이동
    - View 상태 변수를 ViewModel로 이동
    - isFocused는 FocusState 특성상 View에 유지
    - 'vm'을 'viewModel'로 네이밍 통일

--- BusRoad/Feature/MainSearch/MainSearchViewModel.swift
+++ BusRoad/Feature/MainSearch/MainSearchViewModel.swift
@@ -4,24 +4,21 @@ import MapKit
 
 @MainActor
 final class MainSearchViewModel: ObservableObject {
+    @Published var hasSubmitted: Bool = false
+    @Published var isSearchMode: Bool = false
+    @Published var showHint: Bool = false
+    @Published var hasShownVoiceHint: Bool {
+        didSet {
+            UserDefaults.standard.set(hasShownVoiceHint, forKey: kHasShownVoiceHint)
+        }
+    }
+    
+    private let kHasShownVoiceHint = "hasShownVoiceHint_v1"
+    private var bag = Set<AnyCancellable>()
+    
     let searchManager = SearchManager.shared
     let journeyManager = JourneyManager.shared
     
-    @Published var hasSubmitted: Bool = false
-
-    // SearchManager의 변경을 View로 릴레이 (UI 갱신 보장)
-    private var bag = Set<AnyCancellable>()
-    
-    init() {
-        searchManager.$hasSubmitted
-                   .assign(to: &$hasSubmitted)
-        
-        searchManager.objectWillChange
-            .sink { [weak self] _ in self?.objectWillChange.send() }
-            .store(in: &bag)
-    }
-
-    // 뷰에서 쓰기 편한 프록시
     var query: String {
         get { searchManager.query }
         set { searchManager.query = newValue }
@@ -30,7 +27,18 @@ final class MainSearchViewModel: ObservableObject {
     var shouldShowSearchMode: Bool { searchManager.shouldShowSearchMode }
     var isLoading: Bool { searchManager.isLoading }
     var errorMessage: String? { searchManager.errorMessage }
-  
+    
+    
+    init() {
+        self.hasShownVoiceHint = UserDefaults.standard.bool(forKey: kHasShownVoiceHint)
+        searchManager.$hasSubmitted
+            .assign(to: &$hasSubmitted)
+        
+        searchManager.objectWillChange
+            .sink { [weak self] _ in self?.objectWillChange.send() }
+            .store(in: &bag)
+    }
+    
     func search() async { await searchManager.search() }
     func resetSearchMode() { searchManager.resetSearchMode() }
     
@@ -46,4 +54,40 @@ final class MainSearchViewModel: ObservableObject {
         journeyManager.requestOrigin()
     }
     
+    // MARK: - 액션 메서드들
+    /// 검색 모드 종료
+    func exitSearchMode() {
+        query = ""
+        resetManager()
+        isSearchMode = false
+    }
+    
+    /// 검색 수행
+    func performSearch() async {
+        showHint = false
+        isSearchMode = true
+        await search()
+    }
+    
+    /// 검색어 초기화
+    func clearQuery() {
+        showHint = false
+        query = ""
+    }
+    
+    /// 마이크 버튼 탭
+    func handleMicTap() {
+        showHint = false
+    }
+    
+    /// 장소 선택
+    func selectPlace(item: PlaceSummary) {
+        setDestination(destination: LocationInfo(
+            name: item.name,
+            latitude: item.latitude,
+            longitude: item.longitude
+        ))
+        resetManager()
+        isSearchMode = false
+    }
 }
