c69b23267eb798d313f0f418a139075e50742364
Ella <eunseolee90917@gmail.com>
2025-11-03 10:31:55 +0900

    [style]#152 검색결과 하이라이트 색상 변경

--- BusRoad/Component/MainSearchView/PlaceCard.swift
+++ BusRoad/Component/MainSearchView/PlaceCard.swift
@@ -59,7 +59,7 @@ struct PlaceCard: View {
 // MARK: - 텍스트 하이라이트
 extension String {
     /// 검색어와 일치하는 부분을 찾아서 AttributedString으로 변환
-    func highlightedText(searchQuery: String, highlightColor: Color = Color.subPoint) -> AttributedString {
+    func highlightedText(searchQuery: String, highlightColor: Color = Color.blue) -> AttributedString {
         var attributedString = AttributedString(self)
         
         guard !searchQuery.isEmpty else { return attributedString }
