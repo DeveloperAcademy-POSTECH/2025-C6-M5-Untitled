b60a42db812a35202cb5d69044933eeb532d9596
Ella <eunseolee90917@gmail.com>
2025-11-04 10:11:43 +0900

    [style] alert에 배경색상 추가

--- BusRoad/Component/WalkingView/WalkingAlert.swift
+++ BusRoad/Component/WalkingView/WalkingAlert.swift
@@ -81,7 +81,7 @@ struct WalkingAlert: View {
                 
                 .background(
                     RoundedRectangle(cornerRadius: 35)
-                        .fill(.regularMaterial) // 내부 색상
+                        .fill(.alertbackground) // 내부 색상
                         .overlay(
                             RoundedRectangle(cornerRadius: 35)
                                 .stroke(Color.primarywhite, lineWidth: 0.5)
