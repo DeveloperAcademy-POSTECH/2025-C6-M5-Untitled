6ee9337fb3c690f80f2948556c39a6e5cc26af90
Ella <eunseolee90917@gmail.com>
2025-12-30 15:02:13 +0900

    [chore] 코드 주석처리

--- BusRoad/Component/RouteSuggestionView/RouteCard.swift
+++ BusRoad/Component/RouteSuggestionView/RouteCard.swift
@@ -57,49 +57,49 @@ struct RouteCard: View {
 
 
 
-#Preview {
-    @Previewable var previewBusInfo: (busNo: String, arrivalText: String)? = nil
-
-    RouteCard(
-        viewModel: BusRouteViewModel(),
-        allJourneys: [
-            Journey(totalTime: 48, nodes: [
-                .walk(WalkRouteNode(
-                    start: LocationInfo(name: "서울역", latitude: 37.55, longitude: 126.97),
-                    end: LocationInfo(name: "강남역", latitude: 37.49, longitude: 127.02),
-                    travelTime: 5
-                )),
-                .bus(BusRouteNode(
-                    start: LocationInfo(name: "서울역", latitude: 37.55, longitude: 126.97),
-                    end: LocationInfo(name: "강남역", latitude: 37.49, longitude: 127.02),
-                    busNo: ["405", "472"],
-                    busId: [1001, 1002],
-                    stations: [
-                        BusStation(index: 0, stationId: 111, stationName: "서울역", stationCityCode: 1100, localStationId: "LOCAL-SEOUL-001", nodeId: "1000001", latitude: 37.55, longitude: 126.97),
-                        BusStation(index: 1, stationId: 222, stationName: "강남역", stationCityCode: 1100, localStationId: "LOCAL-GANGNAM-001", nodeId: "1000002", latitude: 37.49, longitude: 127.02)
-                    ],
-                    travelTime: 35
-                ))
-            ])
-        ],
-        journey: Journey(totalTime: 48, nodes: [
-            .walk(WalkRouteNode(
-                start: LocationInfo(name: "서울역", latitude: 37.55, longitude: 126.97),
-                end: LocationInfo(name: "강남역", latitude: 37.49, longitude: 127.02),
-                travelTime: 5
-            )),
-            .bus(BusRouteNode(
-                start: LocationInfo(name: "서울역", latitude: 37.55, longitude: 126.97),
-                end: LocationInfo(name: "강남역", latitude: 37.49, longitude: 127.02),
-                busNo: ["472"],
-                busId: [1001],
-                stations: [
-                    BusStation(index: 0, stationId: 111, stationName: "서울역", stationCityCode: 1100, localStationId: "LOCAL-SEOUL-001", nodeId: "1000001", latitude: 37.55, longitude: 126.97),
-                    BusStation(index: 1, stationId: 222, stationName: "강남역", stationCityCode: 1100, localStationId: "LOCAL-GANGNAM-001", nodeId: "1000002", latitude: 37.49, longitude: 127.02)
-                ],
-                travelTime: 35
-            ))
-        ]),
-        index: 0
-    )
-}
+//#Preview {
+//    @Previewable var previewBusInfo: (busNo: String, arrivalText: String)? = nil
+//
+//    RouteCard(
+//        viewModel: BusRouteViewModel(),
+//        allJourneys: [
+//            Journey(totalTime: 48, nodes: [
+//                .walk(WalkRouteNode(
+//                    start: LocationInfo(name: "서울역", latitude: 37.55, longitude: 126.97),
+//                    end: LocationInfo(name: "강남역", latitude: 37.49, longitude: 127.02),
+//                    travelTime: 5
+//                )),
+//                .bus(BusRouteNode(
+//                    start: LocationInfo(name: "서울역", latitude: 37.55, longitude: 126.97),
+//                    end: LocationInfo(name: "강남역", latitude: 37.49, longitude: 127.02),
+//                    busNo: ["405", "472"],
+//                    busId: [1001, 1002],
+//                    stations: [
+//                        BusStation(index: 0, stationId: 111, stationName: "서울역", stationCityCode: 1100, localStationId: "LOCAL-SEOUL-001", nodeId: "1000001", latitude: 37.55, longitude: 126.97),
+//                        BusStation(index: 1, stationId: 222, stationName: "강남역", stationCityCode: 1100, localStationId: "LOCAL-GANGNAM-001", nodeId: "1000002", latitude: 37.49, longitude: 127.02)
+//                    ],
+//                    travelTime: 35
+//                ))
+//            ])
+//        ],
+//        journey: Journey(totalTime: 48, nodes: [
+//            .walk(WalkRouteNode(
+//                start: LocationInfo(name: "서울역", latitude: 37.55, longitude: 126.97),
+//                end: LocationInfo(name: "강남역", latitude: 37.49, longitude: 127.02),
+//                travelTime: 5
+//            )),
+//            .bus(BusRouteNode(
+//                start: LocationInfo(name: "서울역", latitude: 37.55, longitude: 126.97),
+//                end: LocationInfo(name: "강남역", latitude: 37.49, longitude: 127.02),
+//                busNo: ["472"],
+//                busId: [1001],
+//                stations: [
+//                    BusStation(index: 0, stationId: 111, stationName: "서울역", stationCityCode: 1100, localStationId: "LOCAL-SEOUL-001", nodeId: "1000001", latitude: 37.55, longitude: 126.97),
+//                    BusStation(index: 1, stationId: 222, stationName: "강남역", stationCityCode: 1100, localStationId: "LOCAL-GANGNAM-001", nodeId: "1000002", latitude: 37.49, longitude: 127.02)
+//                ],
+//                travelTime: 35
+//            ))
+//        ]),
+//        index: 0
+//    )
+//}
